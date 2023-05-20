77e574fc1112f11458ca7a6ab970518ae374dba0
LeAnne B <branchleannelmb@gmail.com>
2023-05-19 21:10:53 -0400

    allows answers to populate

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ var contentBox = document.querySelector("#contentbox");
 
 //store questions as an array of objects
 
-var myQuestions = [
+var quizQandAs = [
 	{
 		question: "What is 10/2?",
 		answers: {
-			a: '3',
+			a: '7',
 			b: '5',
 			c: '115'
 		},
@@ -26,27 +26,51 @@ var myQuestions = [
 
 //rendering question and its answers onto the page
 function makeQuestion() {
-    //write for loop to do this
-    //for question in myQuestions
-
+    
     //wipe content box clean
     contentBox.innerHTML = "";
 
-    //create new content
-    var newHeader = document.createElement("h1");
-    newHeader.textContent = "Question 1!";
-    newHeader.id = 'question';
-    document.getElementById("contentbox").appendChild(newHeader)
+    //GET THE QUESTION AND ANSWER CONTENT
+        //the question we are on needs to be a variable, value a for loop to cycle through questions
+
+    var currentQuestion = quizQandAs[0];
+    // change above to for loop - for answers under question, make buttons
+
+        //create new question content
+        var newHeader = document.createElement("h1");
+        newHeader.textContent = quizQandAs[0].question;
+        newHeader.id = 'question';
+        document.getElementById("contentbox").appendChild(newHeader)
+
+    //MAKING THE ANSWER BUTTONS
+    var countOfAnswers = (Object.values(currentQuestion.answers).length);
+    var answerArray = Object.values(currentQuestion.answers);
+
+    // console.log(answerArray);
+    // console.log(typeof(answerArray));
+    // console.log(answerArray[2]);
+
+    //prints answers
+    //so what do we need to do with each answer? MAKE BUTTONSSSS instead of logging
+    for (var i = 0; i < countOfAnswers; i++) {
 
     //append answers to the new header 
     var answerElement = document.createElement("button");
     answerElement.id = 'answer';
-    answerElement.innerHTML = "answer1";
+
+    //answerElement.innerHTML = "answer1";
+    answerElement.innerHTML = answerArray[i]
+
     document.getElementById("question").appendChild(answerElement);
+    }
 
-    return ;
+
+
+    
+
+
+return;
 }
-
 
 //write checkAnswer function
 
@@ -60,7 +84,7 @@ function startQuiz() {
     
     makeQuestion();
     //checking for a submitted answer after question has been created
-    document.getElementById("answer").addEventListener('click', checkAnswer);
+    //document.getElementById("answer").addEventListener('click', checkAnswer);
 
     return console.log("the startQuiz function has run.");
 }
---
+++ assets/js/testarea2.js
@@ -0,0 +1,47 @@
+var quizQandAs = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115',
+            d: '110'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+
+console.log("Hello!");
+
+console.log(quizQandAs[0].answers);
+console.log(typeof(quizQandAs[0].answers));
+
+console.log(quizQandAs[0].answers.a);
+console.log(quizQandAs[0].answers.b);
+console.log(quizQandAs[0].answers.c);
+console.log(quizQandAs[0].answers.d);
+
+console.log(quizQandAs[0].answers.keys);
+console.log(quizQandAs[0].answers[0]);
+console.log(Object.values(quizQandAs[0].answers));
+console.log(typeof(Object.values(quizQandAs[0].answers)));
+console.log(quizQandAs[0].answers.values);
+console.log(quizQandAs[0].answers.length);
+//count of answers for the question
+console.log(Object.values(quizQandAs[0].answers).length);
+
+// for (properties in quizQandAs[0].answers) {
+//     var count = count + 1;
+    
+//   }
+  
+// console.log(count);
